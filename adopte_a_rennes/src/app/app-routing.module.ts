700a9fe1332f8928ec8b13f8a0a9a1cf705592ba
MaximeLombard <112693934+MaximeLombard@users.noreply.github.com>
2023-06-01 14:42:44 +0200

    [Initial Routing]

--- adopte_a_rennes/src/app/app-routing.module.ts
+++ adopte_a_rennes/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AcceuilComponent } from './core/features/acceuil/acceuil.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+import { LoginComponent } from './core/auth/login/login.component';
+import { ArticlesComponent } from './core/features/articles/articles.component';
+import { AssociationsComponent } from './core/features/associations/associations.component';
+import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  { path: '', component: AcceuilComponent },
+  { 
+    path: 'auth',
+    children: [
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthGuard], // Apply the auth guard to the dashboard parent route
+    children: [
+      { path: 'articles', component: ArticlesComponent },
+      { path: 'associations', component: AssociationsComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
