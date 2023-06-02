f7a6168498215ade83a0ea5b1e926568b3342707
MaximeLombard <112693934+MaximeLombard@users.noreply.github.com>
2023-06-02 11:18:01 +0200

    Articles fix UI

--- adopte_a_rennes/src/app/models/article.model.ts
+++ adopte_a_rennes/src/app/models/article.model.ts
@@ -5,6 +5,8 @@ export default class ArticleModel {
   description: string | null = null;
   image_id: string | null = null;
   creationDate: string | null = null;
+  firstname: string | null = null;
+  lastname: string | null = null;
   modifiedDate: string | null = null;
   urlImage: string | null = null;
 }
